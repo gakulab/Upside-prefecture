9a6ce0907e3bd03a683994b20f5794aa80a220fe
Keikawamura <49137417+Keikawamura@users.noreply.github.com>
2020-01-29 15:39:20 +0900

    new

---
+++ Reference/Run_Gum_Assessment_Kei.R
@@ -0,0 +1,72 @@
+#' run_gum_assessment
+#'
+#' Run assessment part of global upside model (GUM)
+#' @param dat data frame with at minimum colnames Year,Catch, SciName
+#'
+#' @return results a data frame with assessment results
+#' @export
+run_gum_assessment <- function(dat){
+  
+  
+  #空データの作成
+  temp_predicted <- list()
+  
+  # add in species category info (種分類情報の追加)
+  #列名にSpeciesCat,SpeciesCatName,を含まずにSciNameを含むデータの場合、データにisscaap_linksというデータを付与。
+  if (any(c('SpeciesCat', 'SpeciesCatName') %in% colnames(dat)) == F & 'SciName' %in% colnames(dat)) {
+    #この意味は、SpeciesCat,SpeciesCatNameを持っていない場合には補填するということでろう。
+    dat <- dat %>%
+      left_join(isscaap_links, by = 'SciName')
+    #警告文も付与。種のカテゴリ番号・名前を表す列がないので、FAOデータベースを検索と言っている。
+    warning('No species category number or name provided - looking up from FAO database')
+  }#列名にSpeciesCat,SpeciesCatName,を含まずにSciNameも含まないデータの場合、試行を停止して警告文を発している。
+  else if ((any(c('SpeciesCat', 'SpeciesCatName') %in% colnames(dat)) == F & ('SciName' %in% colnames(dat)) == F)){
+    #最低でもカテゴリ番号・名前、学術名の内一つがないとモデルを試行することができないと言っている
+    stop(" Can't run model without either species catgory number / name (SpeciesCat/SpeciesCatName) and/or scientific name.
+       Provide at least one of these")
+    
+  }
+  
+  # Obtain predicted log B/Bmsy from each model　ー　各モデルから推測された対数B/Bmsyを取得する
+  #見知らぬ関数apply_prm()が出現。
+  for (i in 1 :length(regs) ){
+    temp_predicted[[i]] = apply_prm(dat = dat, reg = regs[[i]]) %>%
+      mutate(model = names(regs[i]), model_number = as.numeric(gsub('M','',model)))
+  }
+  data <- bind_rows(temp_predicted) %>%
+    mutate(model_worked = is.na(LogBvBmsy) == F) %>%
+    filter(model_worked == T) %>% #drop models that didn't work
+    ungroup() %>%
+    group_by(IdOrig) %>%
+    filter(model_number == min(model_number)) %>%
+    mutate(BvBmsy = exp(LogBvBmsy)) %>%
+    rename(year = Year, catch = Catch)#keep the best model that ran for each fishery
+  
+  data <- FindResilience(data) %>%
+    rename(res = Res)
+  
+  stocks <- unique(data$IdOrig)
+  
+  # sub <- sample(stocks, 10, replace = F)
+  #
+  # data <- filter(data, IdOrig %in% sub)
+  #
+  # stocks <- unique(data$IdOrig)
+  #
+  
+  if ('IdOrig' %in% colnames(data)){
+    
+    data$id = data$IdOrig
+    
+  }
+  
+  apply_fun <- function(i,data,stocks){
+    out = run_post_prm_pt_cmsy(dat = filter(data, IdOrig == stocks[i]))$CatchMSY
+  }
+  
+  results <- lapply(1:length(stocks), apply_fun, data = data, stocks = stocks) %>%
+    bind_rows()
+  
+  return(results)
+  
+}
